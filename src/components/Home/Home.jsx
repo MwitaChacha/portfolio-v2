17425b11b436a8b981b5472f73003b3308bd29f0
Mwita Chacha <jonahhchacha@gmail.com>
2022-07-15 08:23:42 +0300

    Adding sample projects to the portfolio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Home.css";
-
+import { motion, useScroll } from 'framer-motion'
 const Home = () => {
   const [quote, setQuote] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -25,12 +25,19 @@ const Home = () => {
   }, []);
 
   if (loading) {
-    return <div>
-        <div className="text-center mt-40 px-10">
-           <h5 className="text-gray-300 text-2xl">"{quote.content}"</h5> 
-           <p>{quote.author}</p>
-        </div>
-    </div>;
+    return (
+      <div>
+        <motion.div
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0}}
+          transition={{ duration: 1 }}
+          className="text-center mt-40 px-10"
+        >
+          <h5 className="text-gray-300 text-2xl">"{quote.content}"</h5>
+          <p>{quote.author}</p>
+        </motion.div>
+      </div>
+    );
   } else {
     return (
       <div>
@@ -51,7 +58,15 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="text-left px-5">
+        <motion.div
+          animate={{
+            y: 0,
+          }}
+          initial={{
+            y: -200,
+          }}
+          className="text-left px-5"
+        >
           <h1 className="text-9xl heading">
             <span className="individual">P</span>
             <span className="individual">H</span>
@@ -66,8 +81,18 @@ const Home = () => {
             <span className="individual">E</span>
             <span className="individual">R</span> -
           </h1>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center ">
+        </motion.div>
+        <motion.div
+          animate={{ y: 0 }}
+          initial={{ y: 300 }}
+          transition={
+            {
+              // spring: 0.5
+            }
+          }
+          whileInView={{ opacity: 1 }}
+          className="grid grid-cols-1 md:grid-cols-2 items-center "
+        >
           <div className="pl-5">
             <h1 className="heading">
               {" "}
@@ -111,6 +136,186 @@ const Home = () => {
                 </svg>
               </div>
             </div>
+          </div>
+        </motion.div>
+
+        {/* Project display section */}
+
+        <div>
+          <div className="p-5">
+            <hr className="my-10" />
+            <p className="text-xs md:text-md">
+              FEATURED
+              <br />
+              PROJECTS
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center">
+            <div>
+              <motion.div
+                whileInView={{
+                  x: 100,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 2,
+                  // repeat: 1
+                }}
+              >
+                <h2 className="project-heading">ARTIST STATS</h2>
+              </motion.div>
+            </div>
+
+            <div className="px-2">
+              <motion.div
+                whileInView={{
+                  x: 0,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1,
+                  // repeat: 1
+                }}
+              >
+                <p className="normal-text">
+                  Ever wanted to know the stats and metadata of your favourite
+                  musician? .
+                </p>
+                <p>DJANGO, TAILWIND CSS, RAPIDAPIs, HEROKU</p>
+              </motion.div>
+            </div>
+          </div>
+          <hr />
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center py-10 flex-col-reverse">
+            <div>
+              <motion.div
+                whileInView={{
+                  x: 0,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 2,
+                  // repeat: 1
+                }}
+              >
+                <h2 className="project-heading pl-2 text-center">CARNNECT</h2>
+              </motion.div>
+            </div>
+            <div className="px-12 md:order-first">
+              <motion.div
+                whileInView={{
+                  x: 0,
+                }}
+                initial={{
+                  x: -100,
+                }}
+                transition={{
+                  duration: 1,
+                  // repeat: 1
+                }}
+              >
+                <p className="normal-text">
+                  This project is an attempt to streamline services in the
+                  automotive industry in Kenya. Connecting drivers, mechanics,
+                  spare part providers and car dealers.
+                </p>
+                <p>DJANGO, BOOTSTRAP, RAPIDAPIs, HEROKU</p>
+              </motion.div>
+            </div>
+          </div>
+          <hr />
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center py-14 ">
+            <div>
+              <motion.div
+                whileInView={{
+                  x: 70,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 2,
+                  // repeat: 1
+                }}
+              >
+                <h2 className="project-heading leading-none">CAL TRACKER </h2>
+              </motion.div>
+            </div>
+
+            <div className="px-10">
+              <motion.div
+                whileInView={{
+                  x: 0,
+                }}
+                initial={{
+                  x: 10,
+                }}
+                transition={{
+                  duration: 1,
+                  // repeat: 1
+                }}
+              >
+                <p className="normal-text">
+                  Get real life calories of your favourite foods from Rapidapi
+                </p>
+                <p>DJANGO, BOOTSTRAP, RAPIDAPIs, HEROKU</p>
+              </motion.div>
+            </div>
+          </div>
+          <hr />
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center py-10 mx-10">
+            <div>
+              <motion.div
+                whileInView={{
+                  x: -50,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 2,
+                  // repeat: 1
+                }}
+              >
+                <h2 className="project-heading pl-2 text-center">BORED</h2>
+              </motion.div>
+            </div>
+            <div className="px-12 md:order-first">
+              <motion.div
+                whileInView={{
+                  x: 20,
+                }}
+                initial={{
+                  x: 0,
+                }}
+                transition={{
+                  duration: 1,
+                  // repeat: 1
+                }}
+              >
+                <p className="normal-text">
+                  This project is give you ideas and activities to do when you are bored.
+                </p>
+                <p>REACT, TAILWIND CSS, FRAMER MOTION, NETLIFY, BOREDAPI</p>
+              </motion.div>
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        <div>
+          <div className="p-5 mt-10">
+            <p className="text-xs md:text-md">
+              INTERESTS &<br />
+              ICE BREAKERS
+            </p>
           </div>
         </div>
       </div>
